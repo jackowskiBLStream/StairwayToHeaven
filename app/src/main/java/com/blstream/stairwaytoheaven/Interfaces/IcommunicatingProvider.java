30854658cfeff96231d40cdaf7050d58ed25f0d3
Patryk Gwiazdowski <pgwiazdowski@gmail.com>
2016-03-21 11:44:55 +0100

    added Icommunication provider

---
+++ app/src/main/java/com/blstream/stairwaytoheaven/Interfaces/IcommunicatingProvider.java
@@ -0,0 +1,22 @@
+package com.blstream.stairwaytoheaven.Interfaces;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Krzysztof Antczak on 21.03.2016.
+ */
+public interface IcommunicatingProvider{
+
+    /**
+     *
+     * @param taskId id of task
+     * @return time elapsed to end of task with id given in parameter
+     */
+    long getElapsedTime(int taskId);
+
+    /**
+     *
+     * @return list of all queued task ids
+     */
+    ArrayList<Integer> getAllTasksId();
+}
