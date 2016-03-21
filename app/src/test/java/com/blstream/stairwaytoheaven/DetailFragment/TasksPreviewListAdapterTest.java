20bafa8f8c0e0efc0cd3528b66d0241cd4304738
Krzysztof Antczak <antek116@gmail.com>
2016-03-21 13:26:12 -0700

    adding new Tests

--- app/src/test/java/com/blstream/stairwaytoheaven/DetailFragment/TasksPreviewListAdapterTest.java
+++ app/src/test/java/com/blstream/stairwaytoheaven/DetailFragment/TasksPreviewListAdapterTest.java
@@ -1,5 +1,12 @@
 package com.blstream.stairwaytoheaven.DetailFragment;
 
+import com.blstream.stairwaytoheaven.Tasks.TaskInformation;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,8 +14,19 @@ import static org.junit.Assert.*;
  * Created by Krzysztof Antczak on 21.03.2016.
  */
 public class TasksPreviewListAdapterTest {
-
-
-
-
+    TasksPreviewListAdapter adapter;
+    ArrayList<TaskInformation> arrayOfTasks;
+    @Before
+    public void setup(){
+        adapter = new TasksPreviewListAdapter();
+        arrayOfTasks = new ArrayList<>();
+    }
+    @Test
+    public void shouldReturnEmptyArray(){
+        //given
+        //when
+        //then
+        assertArrayEquals(arrayOfTasks.toArray(),
+                adapter.getListOfTasks().toArray());
+    }
 }
