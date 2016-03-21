9ed7293fc65594b4f7a0a1777836e72c9c41d6b1
Krzysztof Antczak <antek116@gmail.com>
2016-03-21 10:47:00 -0700

    Add Interface To tasks

---
+++ app/src/main/java/com/blstream/stairwaytoheaven/ComunicatingInterface/ITask.java
@@ -0,0 +1,13 @@
+package com.blstream.stairwaytoheaven.ComunicatingInterface;
+
+
+public interface ITask {
+
+
+    int getTaskId();
+
+    int getTaskDuration();
+
+    int getTaskTime();
+
+}
--- app/src/main/java/com/blstream/stairwaytoheaven/DetailFragment/DetailFragment.java
+++ app/src/main/java/com/blstream/stairwaytoheaven/DetailFragment/DetailFragment.java
@@ -2,4 +2,6 @@ package com.blstream.stairwaytoheaven.DetailFragment;
 
 
 public class DetailFragment {
+
+
 }
