7138d7410e51d003b7979bc71e80084b1468f3fd
Krzysztof Antczak <antek116@gmail.com>
2016-03-21 11:44:19 -0700

    no message

--- app/src/main/java/com/blstream/stairwaytoheaven/DetailFragment/TasksPreviewFragment.java
+++ app/src/main/java/com/blstream/stairwaytoheaven/DetailFragment/TasksPreviewFragment.java
@@ -39,27 +39,27 @@ public class TasksPreviewFragment extends Fragment {
         super.onCreate(savedInstanceState);
         taskPreviewAdapter = new TasksPreviewListAdapter();
 
-        Intent intent  = new Intent(getContext(),)
+//        Intent intent  = new Intent(getContext(),)
 
 
 
     }
-    private ServiceConnection mConnection = new ServiceConnection()
-    {
-
-        @Override
-        public void onServiceConnected(ComponentName className,
-                                       IBinder service) {
-            // We've bound to LocalService, cast the IBinder and get LocalService instance
-            LocalBinder binder = (LocalBinder) service;
-            mService = binder.getService();
-            mBound = true;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName arg0) {
-            mBound = false;
-        }
-    };
+//    private ServiceConnection mConnection = new ServiceConnection()
+//    {
+//
+//        @Override
+//        public void onServiceConnected(ComponentName className,
+//                                       IBinder service) {
+//            // We've bound to LocalService, cast the IBinder and get LocalService instance
+//            LocalBinder binder = (LocalBinder) service;
+//            mService = binder.getService();
+//            mBound = true;
+//        }
+//
+//        @Override
+//        public void onServiceDisconnected(ComponentName arg0) {
+//            mBound = false;
+//        }
+//    };
 
 }
