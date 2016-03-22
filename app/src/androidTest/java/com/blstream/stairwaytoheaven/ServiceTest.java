44cf2ded58b5d5991beee049a2b08e5520c7f29c
Patryk Gwiazdowski <pgwiazdowski@gmail.com>
2016-03-22 13:56:14 +0100

    added tests to service tests

--- app/src/androidTest/java/com/blstream/stairwaytoheaven/ServiceTest.java
+++ app/src/androidTest/java/com/blstream/stairwaytoheaven/ServiceTest.java
@@ -1,8 +1,14 @@
 package com.blstream.stairwaytoheaven;
 
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ServiceTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.blstream.stairwaytoheaven.Service.TaskManagingService;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,11 +20,46 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class ServiceTest {
-
+    TaskManagingService service;
     @Rule
     public final ServiceTestRule mServiceRule = new ServiceTestRule();
+
+    @Before
+    public void setUp() throws Exception{
+
+        Intent serviceIntent =
+                new Intent(InstrumentationRegistry.getTargetContext(),
+                        TaskManagingService.class);
+        IBinder binder = mServiceRule.bindService(serviceIntent);
+
+        service = ((TaskManagingService.LocalBinder) binder).getService();
+    }
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void testSizeOfAllTaskDetailsAfterAddingOne() throws Exception {
+        //Given
+        service.addTask(1,500);
+
+        //When
+        int size = service.getAllTasksDetails().size();
+
+        //Then
+        assertEquals("test msg",1,size);
+    }
+
+    @Test
+    public void testAfterAddingFiveTasksLastShouldHaveDurationTimeAtZero() throws Exception {
+        //Given
+        service.addTask(1,500);
+        service.addTask(1,500);
+        service.addTask(1,500);
+        service.addTask(1,500);
+        service.addTask(1,500);
+
+        //When
+        long progress = service.getAllTasksDetails().size();
+
+        //Then
+      //  assertEquals("test msg",1,size);
     }
 }
--- app/src/main/java/com/blstream/stairwaytoheaven/StartScreen/StartScreenFragment.java
+++ app/src/main/java/com/blstream/stairwaytoheaven/StartScreen/StartScreenFragment.java
@@ -148,7 +148,6 @@ public class StartScreenFragment extends Fragment {
                     taskIdGenerator++;
                 }
 
-
             }
 
         });
